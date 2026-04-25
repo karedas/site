e2d3b3e77c6f22a831f176e7df8face419fda126
Andrea Lisi <lisandr84@gmail.com>
2026-04-25 17:24:09 +0200

    refactor: remove Netlify adapter from Astro configuration
    - Eliminated the Netlify adapter from the Astro configuration file, simplifying the setup.
    - Maintained existing site settings and integrations for React.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://andrea-lisi.com',
   output: 'static',
-  adapter: netlify({ edgeMiddleware: false }),
   integrations: [react()],
   vite: {
     cacheDir: '.vite',
